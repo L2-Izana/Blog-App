a21df564385d4dcf2dd5c3b2066efb5fd13a2760
L2_Izana <dodinhluat6@gmail.com>
2024-03-07 02:49:01 +1100

    Initial commit: Blog App with basic functionalities

---
+++ Blog_App/blog/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+# from . import signals
+
+class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blog'
+    '''
+    def ready(self):
+        return signals
+    '''
